8d3197135d1f0d8cd86275030fca9d10463c5e39
Eddie Welker <eddie.welker@gmail.com>
2012-03-14 23:42:12 -0400

    inital import, through chapter 4

---
+++ guestbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '78695656bb2b316852645baceca37ff854ce905cc0589e653df1ac61ebb753cd2fec53ef4b01aa77f0fecb06f1f7ccbe14cb5572087e1dbbf3e7687b2ea39f72'
---
+++ hello01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = '0f09402546c3ae7f76c76b688ccfaaf2ce9bbc809a73eeec8bbdcf9a9588c07915b4865f66ed3b63c030034d1dce98d8529ef799d4d12fd4fda5ac3cb9a0fbae'
