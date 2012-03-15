f614fe7f6df4794a7cef861c40072757940dc253
Eddie Welker <eddie.welker@gmail.com>
2012-03-15 00:23:08 -0400

    added people w/scaffolding, through the end of chapter 5

---
+++ people/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+People::Application.config.secret_token = 'fe960c099d6ed9dd64c8a538ec29e7325450a79082264c3984338a22b3a077d99ea860584f6a9250ddd872a166388059250235fe2bfa55033e99e909a810858d'
